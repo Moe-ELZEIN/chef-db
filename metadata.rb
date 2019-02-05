5ce8ca43ee9a19d125f129e9175172746be7d06f
Moe El-Zein <melzein@spartaglobal.com>
2019-02-05 23:12:43 +0000

    edit chef db

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,9 @@ long_description 'Installs/Configures mongo'
 version '0.1.0'
 chef_version '>= 13.0'
 
+depends 'mongodb', '= 3.2.20' ;
+depends 'apt'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe "apt"
+include_recipe "mongodb"
+
+apt_repository 'mongodb' do
+  uri        'http://repo.mongodb.org/apt/ubuntu'
+  components ['mongodb']
+end
+
+package "mongodb"
+service "mongodb" do
+  action [:enable, :start]
+end
